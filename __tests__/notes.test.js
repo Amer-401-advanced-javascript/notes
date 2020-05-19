ac0d4c4e1ac6183cd9b405f310972f1677734772
Amer-bit <ny.1995@outlook.com>
2020-05-19 10:28:44 +0300

    convertedtToClass

--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -6,6 +6,8 @@ jest.spyOn(global.console, 'log');
 
 describe('notes module', () => {
   it('execute() with wrong inputs', ()=> {
-    new Execution();
+    let exe = new Execution();
+    exe.add();
+    expect(console.log).not.toHaveBeenCalled;
   });
 });
